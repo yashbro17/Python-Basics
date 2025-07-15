9dd980a2d9d61ac9c59b3dabcf272b15ff6e5e94
yashbro17 <yashanknimesh@gmail.com>
2025-07-15 18:22:36 +0530

    Create q1_variables.py

---
+++ q1_variables.py
@@ -0,0 +1,7 @@
+#Making variables and printing them#
+name = "Yashank Nimesh" #declaring the variables via assignment operator"
+age = "17"
+branch = "Computer science"
+print ("My name is ",name)  #printing the variables using print command#
+print ("My age is ",age)
+print ("My branch is ",branch)
